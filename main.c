97979f437aa3e7b61e1b1a5aafa0e3b68e7b50bf
mael <mael.vernoux@gmail.com>
2026-03-26 15:09:11 +0100

    fonctionnal and first values

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <cpuid.h>
+#include <string.h>
 
 int rdtsc(){
     // Call rdtsc assembly command with a barrier assure by the command cpuid
@@ -24,22 +25,65 @@ int measure_timing_access(int *a){
     int Clock1 = rdtsc();
     *a = *a +1 ;
     int Clock2 = rdtsc();
+
     return Clock2-Clock1;
 }
 
-void measure_cache_hit(int *a){
-    // measure the time if the pointer a is in the cache 
-    for(int i = 0; i < 100; i++){
-        *a = *a +1; // for loop to put the pointer a in the cache 
-    }
-    printf("timing access : %d \n",measure_timing_access(a));
-}
-
-int main(){
+int measure_cache_hit(){
+    // measure the time if a pointer a is in the cache 
     int *a = malloc(sizeof(int));
     *a = 1;
     assert(a != NULL);
 
-    measure_cache_hit(a);
+    for(int i = 0; i < 100; i++){
+        *a = *a +1; // for loop to put the pointer a in the cache 
+    }
+
+    int time = measure_timing_access(a);
+    free(a);
+    return time; 
+}
+
+int main(int argc, char*argv[]){
+    if(argc > 1){
+
+        if (strcmp(argv[1], "cache") == 0){
+            FILE* f = fopen("./access_cache_hit.csv", "w");
+
+            for(int i = 0; i < 10000; i++){
+                fprintf(f, "%d\n",measure_cache_hit());
+            }
+
+            fclose(f);
+        }
+        else{
+            if(strcmp(argv[1], "single")== 0){
+                FILE* f = fopen("./access_single.csv", "w");
+            
+                for(int i = 0; i < 10000; i++){
+                    int *a = malloc(sizeof(int));
+                    *a = 1;
+                    assert(a != NULL);
+
+                    fprintf(f, "%d\n",measure_timing_access(a));
+
+                    free(a);
+                }
+
+                fclose(f);
+            }
+            else{
+                printf("Please enter a measure time access mode.\n");
+                printf("Please enter \"cache\" to get a time access for a pointer in the cache.\n");
+                printf("Please enter \"single\" to get a time access for a pointer access only one time.\n");
+            }
+        }
+    }
+    else{
+        printf("Please enter a measure time access mode.\n");
+        printf("Please enter \"cache\" to get a time access for a pointer in the cache.\n");
+        printf("Please enter \"single\" to get a time access for a pointer access only one time.\n");
+    }
+
     return 0;
 }
---
+++ stat.py
@@ -0,0 +1,3 @@
+import os 
+
+os.system("./time_access cache")
