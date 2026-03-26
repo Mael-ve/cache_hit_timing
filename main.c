211afcc21b2eb003654888c04cdb61c6bfd09c3a
mael <mael.vernoux@gmail.com>
2026-03-26 11:23:17 +0100

    comment and gitignore

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <cpuid.h>
 
 int rdtsc(){
+    // Call rdtsc assembly command with a barrier assure by the command cpuid
+    // Return the actual number cpu cycle of the machine code
     int time;
     asm ("xor %0, %0\n\t"
         "cpuid\n\t"
@@ -18,22 +20,26 @@ int rdtsc(){
 
 
 int measure_timing_access(int *a){
+    // measure the number of cpu cycle to access and increment the value of a 
     int Clock1 = rdtsc();
     *a = *a +1 ;
     int Clock2 = rdtsc();
-    printf("Clock1 : %d\n",Clock1);
-    printf("Clock2 : %d\n",Clock2);
     return Clock2-Clock1;
+}
+
+void measure_cache_hit(int *a){
+    // measure the time if the pointer a is in the cache 
+    for(int i = 0; i < 100; i++){
+        *a = *a +1; // for loop to put the pointer a in the cache 
+    }
+    printf("timing access : %d \n",measure_timing_access(a));
 }
 
 int main(){
     int *a = malloc(sizeof(int));
     *a = 1;
     assert(a != NULL);
-    // for(int i = 0; i < 100; i++){
-    //     *a = *a +1;
-    // }
-    printf("timing access : %d \n",measure_timing_access(a));
-    printf("%d", rdtsc());
+
+    measure_cache_hit(a);
     return 0;
 }
