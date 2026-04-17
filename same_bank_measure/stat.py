8de8c7159320131524b3e5a948d232e2f79087d4
mael <mael.vernoux@gmail.com>
2026-04-17 10:56:11 +0200

    same bank measure accurate

--- same_bank_measure/stat.py
+++ same_bank_measure/stat.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 time_file = pd.read_csv("access_two_value.csv")
 tab = time_file.median(axis=1)
 
-# filter the mean values over than 400 because they are irrelevant 
-plt.hist(tab, rwidth=0.6, bins=100) 
-plt.savefig('../images/same_bank_plot.png')
-plt.show()
+# plt.hist(tab, rwidth=0.6, bins=100) 
+# plt.savefig('../images/same_bank_plot.png')
+# plt.show()
+
+print(len(tab))
